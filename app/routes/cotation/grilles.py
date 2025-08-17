9d37f5ba9a05af640b91868d5250d19229d7a4ce
Julien <verbatim.musicotherapie@gmail.com>
2025-08-17 14:55:32 +0200

    Refonte grilles : nettoyage imports, docstring, organisation PEP8

---
+++ app/routes/cotation/grilles.py
@@ -0,0 +1,23 @@
+"""
+Routes pour la gestion des grilles d'évaluation (standardisées et personnalisées).
+"""
+from flask import Blueprint, render_template
+from flask_login import login_required
+from app.models.cotation import Grille, GrilleDomaine, DomaineIndicateur
+
+grilles_bp = Blueprint('grilles', __name__, url_prefix='/grilles')
+
+@grilles_bp.route('/')
+@login_required
+def grilles():
+    """Affiche toutes les grilles standardisées avec le nombre de domaines et d'indicateurs."""
+    grilles_standardisees = Grille.query.filter_by(type_grille="standardisée", active=True).all()
+    for grille in grilles_standardisees:
+        domaines = GrilleDomaine.query.filter_by(grille_id=grille.id).all()
+        grille.nb_domaines = len(domaines)
+        nb_indicateurs = 0
+        for gd in domaines:
+            indicateurs = DomaineIndicateur.query.filter_by(domaine_id=gd.domaine_id).all()
+            nb_indicateurs += len(indicateurs)
+        grille.nb_indicateurs = nb_indicateurs
+    return render_template('cotation/grilles.html', grilles_standardisees=grilles_standardisees)
