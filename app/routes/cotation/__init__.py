fc4a0cf917fd416c7c7fece96c5ae28f17f0d3b4
Julien <verbatim.musicotherapie@gmail.com>
2025-08-18 02:05:07 +0200

    Fix: ajout et enregistrement du blueprint cotation_bp pour accès aux routes cotation

--- app/routes/cotation/__init__.py
+++ app/routes/cotation/__init__.py
@@ -1,11 +1,11 @@
 from flask import Blueprint
 
-from .grilles import grilles_bp
+## Removed import of grilles_bp (grilles.py deleted)
 from .seances import seances_bp
 from .analytics import analytics_bp
 
 cotation_bp = Blueprint('cotation', __name__, url_prefix='/cotation')
 
-cotation_bp.register_blueprint(grilles_bp)
+## Removed registration of grilles_bp (no longer exists)
 cotation_bp.register_blueprint(seances_bp)
 cotation_bp.register_blueprint(analytics_bp)
