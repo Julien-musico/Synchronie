70003ba9b1ab65f1cc856a7c4f8c0ce3d46654f7
Julien <verbatim.musicotherapie@gmail.com>
2025-08-18 02:46:37 +0200

    Fix: ajout et enregistrement du blueprint cotation_bp pour accès aux routes cotation

--- app/routes/cotation/__init__.py
+++ app/routes/cotation/__init__.py
@@ -1,17 +1,10 @@
-# Route création de grille personnalisée
-@cotation_bp.route('/grilles/creer-grille-personalisee', methods=['GET', 'POST'], endpoint='creer_grille_personalisee')
-def creer_grille_personalisee():
-	"""Affiche le formulaire de création de grille personnalisée."""
-	# TODO: Remplacer par la vraie logique si besoin
-	return render_template('cotation/creer_grille_personalisee.html')
+cotation_bp = Blueprint('cotation', __name__, url_prefix='/cotation')
 from flask import Blueprint, render_template
 
-## Removed import of grilles_bp (grilles.py deleted)
 from .seances import seances_bp
 from .analytics import analytics_bp
 
 cotation_bp = Blueprint('cotation', __name__, url_prefix='/cotation')
-
 
 # Route catalogue des grilles (remplace l'ancien blueprint grilles)
 @cotation_bp.route('/grilles', methods=['GET'])
@@ -19,5 +12,12 @@ def grilles():
 	"""Affiche le catalogue des grilles d'évaluation."""
 	# TODO: Remplacer par la vraie logique si besoin
 	return render_template('cotation/grilles.html')
+
+# Route création de grille personnalisée
+@cotation_bp.route('/grilles/creer-grille-personalisee', methods=['GET', 'POST'], endpoint='creer_grille_personalisee')
+def creer_grille_personalisee():
+	"""Affiche le formulaire de création de grille personnalisée."""
+	# TODO: Remplacer par la vraie logique si besoin
+	return render_template('cotation/creer_grille_personalisee.html')
 cotation_bp.register_blueprint(seances_bp)
 cotation_bp.register_blueprint(analytics_bp)
