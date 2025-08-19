34ce7a04e81b5eda8c9424d8f215c9fcd673fd5a
Julien <verbatim.musicotherapie@gmail.com>
2025-08-19 22:43:19 +0200

    modification grilles.html

--- app/routes/cotation/__init__.py
+++ app/routes/cotation/__init__.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from app.models.cotation import GrilleEvaluation
+from flask_login import current_user
 
 from .seances import seances_bp
 from .analytics import analytics_bp
@@ -9,8 +11,15 @@ cotation_bp = Blueprint('cotation', __name__, url_prefix='/cotation')
 @cotation_bp.route('/grilles', methods=['GET'])
 def grilles():
     """Affiche le catalogue des grilles d'évaluation."""
-    # TODO: Remplacer par la vraie logique si besoin
-    return render_template('cotation/grilles.html')
+    grilles_standard = GrilleEvaluation.query.filter_by(type_grille='standard', active=True).all()
+    grilles_perso = []
+    if current_user.is_authenticated:
+        grilles_perso = GrilleEvaluation.query.filter_by(type_grille='personnalisee', user_id=current_user.id, active=True).all()
+    return render_template(
+        'cotation/grilles.html',
+        grilles_standard=grilles_standard,
+        grilles_perso=grilles_perso
+    )
 
 # Route création de grille personnalisée
 @cotation_bp.route('/grilles/creer-grille-personalisee', methods=['GET', 'POST'], endpoint='creer_grille_personalisee')
